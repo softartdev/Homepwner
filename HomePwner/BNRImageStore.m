a929225dfaae83d98a3ad90731f35c135e22b741
artur <artik222012@gmail.com>
2018-04-01 22:58:46 +0500

    chapter 18 complete

--- HomePwner/BNRImageStore.m
+++ HomePwner/BNRImageStore.m
@@ -47,8 +47,17 @@
     if (self) {
         _dictionary = [[NSMutableDictionary alloc] init];
     }
+    
+    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+    [nc addObserver:self selector:@selector(clearCache:) name:UIApplicationDidReceiveMemoryWarningNotification object:nil];
 
     return self;
+}
+
+- (void)clearCache:(NSNotification *)note
+{
+    NSLog(@"flushing %d images out of the cache", [self.dictionary count]);
+    [self.dictionary removeAllObjects];
 }
 
 - (void)setImage:(UIImage *)image forKey:(NSString *)key
