285433f0a39ebaab0479f58e779b73d7fd0d71e9
artur <artik222012@gmail.com>
2018-04-02 14:54:34 +0500

    Image Manipulation

--- HomePwner/BNRDetailViewController.m
+++ HomePwner/BNRDetailViewController.m
@@ -250,6 +250,8 @@ didFinishPickingMediaWithInfo:(NSDictionary *)info
 
     // Get picked image from info dictionary
     UIImage *image = info[UIImagePickerControllerOriginalImage];
+    
+    [self.item setThumbnailFromImage:image];
 
     // Store the image in the BNRImageStore for this key
     [[BNRImageStore sharedStore] setImage:image forKey:self.item.itemKey];
--- HomePwner/BNRItem.m
+++ HomePwner/BNRItem.m
@@ -99,6 +99,7 @@
     [aCoder encodeObject:self.serialNumber forKey:@"serialNumber"];
     [aCoder encodeObject:self.dateCreated forKey:@"dateCreated"];
     [aCoder encodeObject:self.itemKey forKey:@"itemKey"];
+    [aCoder encodeObject:self.thumbnail forKey:@"thumbnail"];
     [aCoder encodeInt:self.valueInDollars forKey:@"valueInDollars"];
 }
 
@@ -110,9 +111,29 @@
         _serialNumber = [aDecoder decodeObjectForKey:@"serialNumber"];
         _dateCreated = [aDecoder decodeObjectForKey:@"dateCreated"];
         _itemKey = [aDecoder decodeObjectForKey:@"itemKey"];
+        _thumbnail = [aDecoder decodeObjectForKey:@"thumbnail"];
         _valueInDollars = [aDecoder decodeIntForKey:@"valueInDollars"];
     }
     return self;
+}
+
+- (void)setThumbnailFromImage:(UIImage *)image
+{
+    CGSize origImageSize = image.size;
+    CGRect newRect = CGRectMake(0, 0, 40, 40);
+    float ratio = MAX(newRect.size.width / origImageSize.width, newRect.size.height / origImageSize.height);
+    UIGraphicsBeginImageContextWithOptions(newRect.size, NO, 0.0);
+    UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:newRect cornerRadius:5.0];
+    [path addClip];
+    CGRect projectRect;
+    projectRect.size.width = ratio * origImageSize.width;
+    projectRect.size.height = ratio * origImageSize.height;
+    projectRect.origin.x = (newRect.size.width - projectRect.size.width) / 2;
+    projectRect.origin.y = (newRect.size.height - projectRect.size.height) / 2;
+    [image drawInRect:projectRect];
+    UIImage *smallImage = UIGraphicsGetImageFromCurrentImageContext();
+    self.thumbnail = smallImage;
+    UIGraphicsEndImageContext();
 }
 
 @end
